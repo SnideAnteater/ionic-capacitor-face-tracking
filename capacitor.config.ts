332a7f585bd2eafc38e5b13b75becc853d32724c
Akmal Yassin <46955396+SnideAnteater@users.noreply.github.com>
2024-05-30 14:31:21 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'native-face-filter',
+  webDir: 'www'
+};
+
+export default config;
